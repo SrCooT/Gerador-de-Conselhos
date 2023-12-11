e795934e543d7a845e0a429e55d3a7aceb5eb0b4
Juno Crisitan <juno.lp@hotmail.com>
2023-12-10 21:19:51 -0300

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
+const titulo = document.querySelector(".title")
+const mensage = document.querySelector(".frase")
 const btn = document.getElementById('botao');
 
 
-btn.addEventListener("click", ()=>{
-    return conselho((url = 'https://api.adviceslip.com/advice') => {
-        
-    })
-})
+console.log(mensage);
+btn.addEventListener("click",
+async function conselhoAleatoria(){
+    const  response = await fetch("https://api.adviceslip.com/advice")
+    const conselhoGerado = await response.json()
+
+
+    console.log(conselhoGerado.slip.advice);
+    console.log(conselhoGerado.slip.id);
+
+    mensage.textContent = conselhoGerado.slip.advice
+    titulo.textContent = `a d v i c e # ${conselhoGerado.slip.id}`
+}
+);
 
